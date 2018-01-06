74d572b25e42ea928984080a2de24bbbddce98f1
Aidan Lee <aidan.lee63@gmail.com>
2018-01-06 18:49:13 +0000

    Removed unneeded function typedefs

--- linc/linc_discord_rpc.h
+++ linc/linc_discord_rpc.h
@@ -1,20 +1,27 @@
 #pragma once
 
 #include <hxcpp.h>
-#include "discord-rpc.h"
+#include "../lib/discord-rpc/include/discord-rpc.h"
 
 namespace linc
 {
     namespace discord_rpc
     {
-        typedef ::cpp::Function <void(void)> VoidCallback;
-        typedef ::cpp::Function <void(int, const char*)> ErrorCallback;
-        typedef ::cpp::Function <void(const char*)> SecretCallback;
-        typedef ::cpp::Function <void(const DiscordJoinRequest*)> JoinRequestCallback;
-
-        extern void init(const char* _clientID, const char* _steamAppID, VoidCallback, ErrorCallback, ErrorCallback, SecretCallback, SecretCallback, JoinRequestCallback);
+        extern void init(const char* _clientID, const char* _steamAppID,
+            void (*_onReady)(),
+            void (*_onDisconnected)(int errorCode, const char* message),
+            void (*_onError)(int errorCode, const char* message),
+            void (*_onJoin)(const char* joinSecret),
+            void (*_onSpectate)(const char* spectateSecret),
+            void (*_onRequest)(const DiscordJoinRequest* request));
         extern void process();
-        extern void update_presence(const char*, const char*, int64_t, int64_t, const char*, const char*, const char*, const char*, const char*, int, int, const char*, const char*, const char*, int8_t);
+        extern void update_presence(
+            const char* _state, const char* _details,
+            int64_t _startTimestamp, int64_t _endTimestamp,
+            const char* _largeImageKey, const char* _largeImageText,
+            const char* _smallImageKey, const char* _smallImageText,
+            const char* _partyID, int _partySize, int _partyMax, const char* _matchSecret, const char* _joinSecret, const char* _spectateSecret,
+            int8_t _instance);
         extern void respond(const char* _userID, int _reply);
         extern void shutdown();
     }
